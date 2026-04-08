fe0c1dd0e322775557d7171e8a4bde1de4153f9a
Pavel Kuzmin <mpakunderscore@gmail.com>
2026-04-08 10:54:32 -0700

    feat(client): add frontend shell and styles

---
+++ src/main/App.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react'
+
+// import { Footer } from '@src/main/views/Footer'
+// import { Header } from '@src/main/views/Header'
+
+const App = () => {
+    return (
+        <main className={'main'}>
+            {/*<Header />*/}
+            {/* sections here */}
+            {/*<Footer />*/}
+        </main>
+    )
+}
+
+export default App
---
+++ src/utils/global.d.ts
@@ -0,0 +1,8 @@
+declare const VERSION: string
+declare const GIT: string
+declare const BRANCH: string
+declare const BUILD_TIME: string
+declare const NODE_ENV: string
+declare const PORT: string
+
+declare module '*.css'
